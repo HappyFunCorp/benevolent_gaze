e3494cd23b2babb36a90e0a69b93f44276f435d7
HappyFunCorp <happyfuncorp@Wills-Mac-mini.home>
2014-12-09 14:06:25 -0500

    Javascript change to deal with null names.

---
+++ redis-2.0.0-rc1/tests/support/test.tcl
@@ -0,0 +1,33 @@
+set ::passed 0
+set ::failed 0
+set ::testnum 0
+
+proc test {name code okpattern} {
+    incr ::testnum
+    # if {$::testnum < $::first || $::testnum > $::last} return
+    puts -nonewline [format "#%03d %-68s " $::testnum $name]
+    flush stdout
+    if {[catch {set retval [uplevel 1 $code]} error]} {
+        puts "ERROR\n\nLogged warnings:"
+        foreach file [glob tests/tmp/server.[pid].*/stdout] {
+            set warnings [warnings_from_file $file]
+            if {[string length $warnings] > 0} {
+                puts $warnings
+            }
+        }
+        exit 1
+    }
+    if {$okpattern eq $retval || [string match $okpattern $retval]} {
+        puts "PASSED"
+        incr ::passed
+    } else {
+        puts "!! ERROR expected\n'$okpattern'\nbut got\n'$retval'"
+        incr ::failed
+    }
+    if {$::traceleaks} {
+        if {![string match {*0 leaks*} [exec leaks redis-server]]} {
+            puts "--------- Test $::testnum LEAKED! --------"
+            exit 1
+        }
+    }
+}
