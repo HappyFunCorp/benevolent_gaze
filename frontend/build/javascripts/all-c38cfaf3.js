a5955ed652515e4b649400c3ebeaecaaace647f8
Aaron Brocken <aaronbrocken@gmail.com>
2014-12-12 15:15:20 -0500

    Revise javascript

--- frontend/build/javascripts/all-c38cfaf3.js
+++ frontend/build/javascripts/all-c38cfaf3.js
@@ -12109,8 +12109,8 @@ es.onmessage = function(e) {
 
 es.addEventListener('message', function(e) {
   new_people = JSON.parse(e.data)
-  rename_users(new_people);
   add_remove_workers(new_people);
+  check_last_seen();  
 }, false);
 
 es.addEventListener('open', function(e) {
@@ -12127,27 +12127,49 @@ es.addEventListener('error', function(e) {
 var w;
 
 var Worker = {
+  setup_and_add: function(worker_object) {
+          var klass = worker_object.device_name.replace(/\./g, "");
+          Worker.grab_worker();
+          Worker.set_avatar(worker_object.avatar);
+          Worker.set_name(worker_object);
+          Worker.add_class("."+klass);
+          Worker.add_to_board(worker_object);
+        },
   grab_worker: function(){
                  w = $('.worker').first().clone();
                },
   set_image: function(string){
     $('img', w).attr('src', string);
   },
-  set_name: function(string){
-              $('.tape', w ).text(string);
+  set_name: function(worker_data){
+              $('.tape', w ).text(sanitize_name( worker_data.name || worker_data.device_name));
+              $(w).attr("data-name", (worker_data.name || worker_data.device_name));
+              $(w).attr("data-devicename", worker_data.device_name);
             },
   set_avatar: function(avatar_url){
                 $('.avatar_container img', w).attr('src', avatar_url || "/images/visitor_art@1x-21d82dcb.png");
               },
-  add_class: function(string){
-               w.addClass(string.replace(/\./g, ""));
+  add_class: function(device_name){
+               w.addClass(device_name.replace(/\./g, ""));
              },
-  add_to_board: function(){
+  add_to_board: function(worker_data){
+                  Welcome.move_logo_and_welcomes();
                   $(w).children('.pin_and_avatar_container').addClass("animated").addClass("swing" + (Math.floor(((Math.random() * 2) + 1))).toString());
                   $('.right_column .row').append( w );
+                  $('.newcomer h3').text(worker_data.name || sanitize_name(worker_data.device_name));
+                  $('.newcomer_avatar img').attr('src', worker_data.avatar || "/images/visitor_art@1x-21d82dcb.png");
+                  $('.newcomer_avatar, .newcomer').show().removeClass('animated').removeClass('bounceOutUp').addClass('animated bounceInDown');
+                  $('.newcomer_avatar, .newcomer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e) {
+                    $(this).removeClass('bounceInDown').addClass('bounceOutUp');
+                    Worker.redraw();
+                  })
                 },
   remove_worker: function(k) {
                    $( k ).addClass("animated bounceOutDown");
+                   $('.bounceOutDown').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e) {
+                     $(this).remove();
+                     Worker.redraw();
+                   });
                  },
   redraw: function() {
             var w = $('.worker').length;
@@ -12165,70 +12187,27 @@ var Worker = {
 
 var add_remove_workers = function(w){
 
-  var temp_name_arr = [];
-
   w.map(function(worker_data){
-    var klass = worker_data.device_name.replace(/\./g, "");
-    name_presence = current_people_name_array.indexOf(worker_data.name);
-    name_in_temp_arr = temp_name_arr.indexOf(worker_data.name);
-    if($("."+klass).length > 0 || current_people_name_array.indexOf(worker_data.name) >= 0 || temp_name_arr.indexOf(worker_data.name) >= 0) {
-      console.log("Nobody new.");
-    } else {
-      if (worker_data.name) {
-        temp_name_arr.push(worker_data.name);
+    data_attribute = "[data-name='" + (worker_data.name || worker_data.device_name) + "']";
+    data_attribute_device = "[data-name='" + worker_data.device_name + "']";
+    $element = $(data_attribute);
+    console.log(data_attribute);
+    console.log($element);
+    console.log($element.length);
+    if ($element.length > 0) {
+      $element.attr("data-lastseen", $.now());
+      change_avatar(worker_data, data_attribute);
+      data_attribute_worker = "[data-devicename='" + worker_data.device_name + "']";
+      if ( $(data_attribute_worker).find(".tape").text() !== ( worker_data.name || sanitize_name(worker_data.device_name) ) ) {
+        Worker.remove_worker(data_attribute_worker);
       }
-      Welcome.move_logo_and_welcomes();
-      current_people.push(worker_data);
-      Worker.grab_worker();
-      Worker.set_avatar(worker_data.avatar);
-      Worker.set_name(worker_data.name || sanitize_name(worker_data.device_name));
-      Worker.add_class("."+klass);
-      Worker.add_to_board();
-      $('.newcomer h3').text(worker_data.name || sanitize_name(worker_data.device_name));
-      $('.newcomer_avatar img').attr('src', worker_data.avatar || "/images/visitor_art@1x-21d82dcb.png");
-      $('.newcomer_avatar, .newcomer').show().removeClass('animated').removeClass('bounceOutUp').addClass('animated bounceInDown');
-      $('.newcomer_avatar, .newcomer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e) {
-        $(this).removeClass('bounceInDown').addClass('bounceOutUp');
-        Worker.redraw();
-      })
-    }
-  });
-  
-  current_people = current_people.filter(function(worker){
-    var last_seen_plus_fifteen_mins = parseInt(worker.last_seen) + 900000;
-    
-    console.log($.now() - last_seen_plus_fifteen_mins);
-
-    if (last_seen_plus_fifteen_mins <= $.now()) {
-      var klass = worker.device_name.replace(/\./g, "");
-      Worker.remove_worker("."+klass);        
-      $('.bounceOutDown').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e) {
-        $(this).remove();
-        Worker.redraw();
-      });
-    };
-    return !(last_seen_plus_fifteen_mins <= $.now());
-  });
-  
-  current_people = current_people.map(function(cp){
-    updated_person = cp;
-     new_people.map(function(np){
-       if (cp.device_name == np.device_name) {
-         updated_person = np;
-       }
-       //if there is a name match this still updates the last_seen time on the other device so that it doesn't go stale.
-       if (cp.name == np.name && cp.name && np.name) {
-         cp.last_seen = np.last_seen;
-         updated_person = cp;
-       }
-     });
-     return updated_person;
-  });
-
-  current_people_name_array = current_people.map(function(cp){
-    if (cp.name) {
-      return cp.name;
-    }
+    } else {
+      Worker.setup_and_add(worker_data);
+      if (worker_data.name) {
+        Worker.remove_worker(data_attribute_device);
+      }
+    }   
+      
   });
 
 };
@@ -12246,27 +12225,20 @@ var sanitize_name = function(name){
       return name_change
 };
 
-var rename_users = function(user_names) {
-  user_names.map(function(u){
-    var name_klass = u.device_name.replace(/\./g, "");
-    var $el = $('.'+name_klass);
-    u_name = u.name || sanitize_name(u.device_name);
-    $el.children('.tape').text(u_name);
-    change_avatar(u,name_klass);
-  });
+var check_last_seen = function() {
+  $('.worker').each(function(wk){
+    if ($(wk).attr('data-lastseen') < ($.now() - 90000)) {
+      Worker.remove_worker(wk);
+    }
+  })
 }
 
-var change_avatar = function(user_param, klass){
-
-  var element = $('.'+klass).find('.avatar_container img')
+var change_avatar = function(user_param, data_attribute){
+  var element = $(data_attribute).find('.avatar_container img')
   if (typeof user_param.avatar == "string" && user_param.avatar != element.attr('src')) {
-    //if (klass.match(/iP/) && !element.hasClass('rotate90')) {
-      //$('.'+klass).find(".avatar_container img").addClass("rotate90");
-    //}
-    $('.'+klass).find(".avatar_container img").attr('src',user_param.avatar);
+    $(data_attribute).find(".avatar_container img").attr('src',user_param.avatar);
   }
 };
-
 
 var Welcome = {
   move_logo_and_welcomes: function() {
@@ -12277,6 +12249,6 @@ var Welcome = {
                   });
                }
   
-}
+};
 
 });
